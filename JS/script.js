8d15af548c8c2a2b0b9f82a61191021c03a8f2fc
EAF91 <emilio.flores@cix.csi.cuny.edu>
2021-12-09 23:59:00 -0500

    Task 8

--- JS/script.js
+++ JS/script.js
@@ -36,12 +36,13 @@ console.log(grades, boolGrade)
 function germanNumbers(){
     const cardinalNumbers = [2,3,4,5,6];
     const germanNumbers = ['zwei', 'drei', 'vier', 'fünf', 'sechs'];
+    cardinalNumbers.map();
 
 }
 
 //------------------------------------------------------------
 function returnPrimeNumbers(){
     const numbers = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15];
-
+    const onlyPrime = numbers;
 }
 
